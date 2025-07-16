db1bccc33904b86ef83693336643a8eca6755129
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-07-16 19:30:32 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+const App = () => {
+  return (
+    <>
+      <h1 className="data">hello guys</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum
+      </p>
+
+      <h1>form</h1>
+      name : <input type="text" />  <br />  <br />
+      age : <input type="number" />  <br />  <br />
+      city : <input type="text" />  <br />  <br />
+    
+      <hr />
+      <button>click me</button>
+
+      </>
+  )
+}
+export default App;
---
+++ src/Cybrom.jsx
@@ -0,0 +1,6 @@
+const Cybrom = () => {
+  return(
+    <h1>welcome to my Cybrom we learn react</h1>
+  )
+}
+export default Cybrom;
