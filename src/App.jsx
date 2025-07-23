22b418e717aab7d13726e3c9ff0f71932883b4ea
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-07-23 19:33:25 +0530

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import Carousel from 'react-bootstrap/Carousel';
+
 import img1 from "./images/r1.png";
 import img2 from "./images/r2.png";
 import img3 from "./images/r3.png";
 import img4 from "./images/r4.png";
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 const App = () => {
   return (
     <>
-       <Navbar bg="light" data-bs-theme="light">
+       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
@@ -22,10 +27,45 @@ const App = () => {
         </Container>
       </Navbar>
 
-      <img src={img1} width="300" height="200"/>
-      <img src={img2} width="300" height="200" />
-      <img src={img3} width="300" height="200"/>
-      <img src={img4} width="300" height="200"/>
+
+       <Carousel>
+      <Carousel.Item>
+        <img src={img1} width="100%" height="500" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img2} width="100%" height="500"  />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+       <img src={img3} width="100%" height="500"/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+     <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={img4} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </>
   )
 }
