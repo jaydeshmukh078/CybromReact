31e692cb5228752e4f4e74267a30c4a7ced3568d
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-07-24 19:20:05 +0530

    event

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,16 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-import Carousel from 'react-bootstrap/Carousel';
-
-import img1 from "./images/r1.png";
-import img2 from "./images/r2.png";
-import img3 from "./images/r3.png";
-import img4 from "./images/r4.png";
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-
 const App = () => {
+
+const display=(e)=>{
+  let mytype=e.type;
+  let myname=e.target.name;
+  let myvalue=e.target.value;
+  alert(`name : ${myname} type : ${mytype} value : ${myvalue}`);
+}
+
   return (
     <>
-       <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-
-       <Carousel>
-      <Carousel.Item>
-        <img src={img1} width="100%" height="500" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src={img2} width="100%" height="500"  />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-       <img src={img3} width="100%" height="500"/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-
-     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img4} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+       <h1>Welcome to Cybrom</h1>
+       <button name="btn1" value="mybtn1" onClick={display}>Click Here!</button> 
     </>
   )
 }
