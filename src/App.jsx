47db2d569820a0f6cd0a5a2918dc260dd7d9d339
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-07-26 19:30:08 +0530

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-const [Color, setColor] = useState("red");
+  const [count, setCount] = useState(0);
+  const [multi, setMulti] = useState(0);
 
+  useEffect(()=>{
+    setMulti(count*2)
+  },[count])
 
   return (
     <>
-       <h1 style={{color:Color}}>My Fav Color is {Color}</h1>
-       <button onClick={()=>{setColor("Green")}}>Green</button>
-       <button onClick={()=>{setColor("Blue")}}>Blue</button>
-       <button onClick={()=>{setColor("Pink")}}>Pink</button>
-       <button onClick={()=>{setColor("Orange")}}>Orange</button>
-       <button onClick={()=>{setColor("yellow")}}>yellow</button>
-       <button onClick={()=>{setColor("Black")}}>Black</button>
+      <h1>My Count : {count}</h1>
+      <h2>Multiplication : {multi}</h2>
+      <button onClick={() => setCount(count + 1)}>Click Here!</button>
     </>
   )
 }
-
 export default App;
