feba08fa3e8e0d5d7b837d1189cef6eab7b9ae77
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-08-01 19:25:29 +0530

    beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
+import axios from "axios";
 import { useState } from "react";
 
 const App = () => {
-  const [Name, setName] = useState("");
-  const [City, setCity] = useState("");
+  const [input, setInput] = useState("");
+ 
+  const handleInput=(e)=>{
+    let name= e.target.name 
+    let value= e.target.value
 
-  const handleSubmit = () => {
-    console.log("Form submitted");
-    console.log({ handlename: Name, handlecity: City });
+    setInput(values=>({...values, [name]:value}))
+    console.log(input);
+    
+  }
 
+  const handleSubmit = async() => {
+    let api= "http://localhost:3000/student"
+    const response = await axios.post(api, input)
+    console.log(response);
+    alert("data sent successfully")
   }
 
   return (
     <>
       <h1>Application Form</h1>
 
-      Enter Name : <input type="text" value={Name} onChange={(e) => setName(e.target.value)} />
+      Enter Name : <input type="text"  name="name" onChange={handleInput} />
       <br />
-      Enter City : <input type="text" value={City} onChange={(e) => setCity(e.target.value)} />
+      Enter City : <input type="text" name="city"  onChange={handleInput} />
+      <br />
+      Enter Rollno. : <input type="text" name="rollno"  onChange={handleInput} />
+      <br />
+      Enter Fees : <input type="text" name="fee"  onChange={handleInput} />
       <br />
       <button onClick={handleSubmit}>Submit</button>
     </>
