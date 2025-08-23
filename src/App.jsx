0d698ee4c85dd0663c344333cd7a3cbbe2667e6d
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-08-23 19:34:26 +0530

    alfaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -37,19 +37,90 @@
 // export default App;
 
 
-import { useRef, useState,useEffect } from "react";
-const App=()=>{
-  const [txtval ,setTxtval] =useState("")
- const myref = useRef(0);
+// import { useRef, useState,useEffect } from "react";
+// const App=()=>{
+//   const [txtval ,setTxtval] =useState("")
+//  const myref = useRef(0);
  
-useEffect(()=>{
-  myref.current=myref.current+1;
-})
+// useEffect(()=>{
+//   myref.current=myref.current+1;
+// })
+//   return (
+//     <>
+//    Enter name : <input type="text" value={txtval} onChange={(e)=>{setTxtval(e.target.value)}}/>
+//    <h1>My render Count : {myref.current}</h1>
+    
+//     </>
+//   );
+// };
+// export default App;
+
+
+// import { useReducer} from "react";
+
+// const App=()=>{
+
+//   const myMethod=(state,action)=>{
+//     switch(action) {
+//       case "Inc":
+//         return state+1;
+
+//       case "Dec":
+//         return state-1;
+
+//       default :
+//         return state;
+
+//     }
+//   }
+
+//   const [count, dispatch]= useReducer(myMethod,0);
+
+//   return (
+//     <>
+//       <h1>My Counter Programer Using useReducer</h1>
+//       <hr />
+
+//       <button onClick={()=>{dispatch("Inc")}}> Increment </button>
+//       <h1>Count : {count}</h1>
+//       <button onClick={()=>{dispatch("Dec")}}> Decrement </button>
+//     </>
+//   );
+// };
+// export default App;
+
+
+import { useReducer} from "react";
+const App=()=>{
+  const myMethod=(state,action)=>{
+    switch(action) {
+      case "red":
+        return state="red";
+
+      case "green":
+        return state="green";
+
+      case "blue":
+        return state="blue";
+
+      case "yellow":
+        return state="yellow";
+
+      default :
+        return state;
+    }
+  }
+  const [color, dispatch]= useReducer(myMethod,"pink");
   return (
     <>
-   Enter name : <input type="text" value={txtval} onChange={(e)=>{setTxtval(e.target.value)}}/>
-   <h1>My render Count : {myref.current}</h1>
-    
+      <h1>My Color Change Program Using useReducer</h1>
+      <hr />
+      <button onClick={()=>{dispatch("red")}}> Red </button>
+      <button onClick={()=>{dispatch("green")}}> green </button>
+      <button onClick={()=>{dispatch("blue")}}> blue </button>
+      <button onClick={()=>{dispatch("yellow")}}> yellow </button>
+      <div style={{backgroundColor:color, width:300, height:300}}>
+      </div>
     </>
   );
 };
